09ec6693e0b46bf1fce9accaf582e422bd24aafa
pauleveritt <paul.everitt@jetbrains.com>
2025-05-03 13:44:02 -0400

    Run Playwright tests in Python with pytest-playwright. Install a network interceptor to avoid an HTTP connection. Instead, serve anything under http://fake/ from tests/stubs.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,1 @@
-try:
-    # Hack to work around greenlet not compiling in GitHub Actions for
-    # Python 3.14.
-    import playwright
-
-    pytest_plugins = "tdom.fixtures"
-except ImportError:
-    pass
+pytest_plugins = "tdom.fixtures"
