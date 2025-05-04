497c1d34e3028a826cacf7a21d86e7a0873b297e
pauleveritt <paul.everitt@jetbrains.com>
2025-05-04 09:58:26 -0400

    PyScript is unhappy when not running against localhost:8000 so change the faking strategy.
    Remove the Playwright/JS setup. Write a single example of Playwright-Python-Pyodide.

--- tests/index.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:8000/tests/index.html');
-  await page.waitForSelector('body.done');
-  const text = await page.textContent('body.done');
-  expect(text).toBe('OK');
-});
