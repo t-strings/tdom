871445fe3ef5e886f1ce8cf689cf792fce8f83e7
pauleveritt <paul.everitt@jetbrains.com>
2025-05-04 09:58:26 -0400

    PyScript is unhappy when not running against localhost:8000 so change the faking strategy.
    Remove the Playwright/JS setup. Write a single example of Playwright-Python-Pyodide.

--- src/tdom/fixtures.py
+++ src/tdom/fixtures.py
@@ -11,8 +11,8 @@ import pytest
 from playwright.sync_api import Page
 from playwright.sync_api import Route
 
-STUBS = Path(__file__).parent.parent.parent / "tests" / "pwright" / "stubs"
-
+ROOT = Path(__file__).parent.parent.parent
+STUBS = ROOT / "tests" / "pwright" / "stubs"
 
 @dataclass
 class DummyResponse:
@@ -69,14 +69,19 @@ class DummyPage:
 
 
 def route_handler(page: Page, route: Route) -> None:
-    """Called from the interceptor to get the data off disk."""
+    """Called from the interceptor to get the data off of the disk."""
     this_url = urlparse(route.request.url)
-    this_path = this_url.path[1:]
-    is_fake = this_url.hostname == "fake"
-    headers = dict()
+    is_fake = (this_url.hostname == "localhost") and (this_url.port == 8000)  # this_url.path.startswith("/fake/")
     if is_fake:
         # We should read something from the filesystem
-        this_fs_path = STUBS / this_path
+        headers = dict()
+        this_path = this_url.path[1:]
+        if this_path.startswith("src"):
+            # Load relative to the src directory
+            this_fs_path = ROOT / this_path
+        else:
+            # This should be under tests/stubs
+            this_fs_path = STUBS / this_path
         if this_fs_path.exists():
             status = 200
             mime_type = guess_type(this_fs_path)[0]
