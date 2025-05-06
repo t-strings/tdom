e6468b75fe60710c3792ae48923849292cbf0589
pauleveritt <paul.everitt@jetbrains.com>
2025-05-06 15:35:34 -0400

    Remove use of Playwright tests.

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-try:
-    # Hack to work around greenlet not compiling in GitHub Actions for
-    # Python 3.14.
-    import playwright
-
-    pytest_plugins = "tdom.fixtures"
-except ImportError:
-    pass
