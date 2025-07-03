f6f72cfdb4983a75efffe3dc88eb909e68cfd255
pauleveritt <pauleveritt@me.com>
2025-07-03 15:48:42 +0200

    First stab at tests.

---
+++ examples/test_micropython_examples.py
@@ -0,0 +1,20 @@
+"""Use Playwright to test examples in MicroPython in a browser."""
+from playwright.sync_api import Page, expect
+
+
+# Use `PWDEBUG=1` to run "headful" in the Playwright test app
+# @pytest.mark.skip(reason="not implemented")
+def test_static_string(fake_page: Page):
+    url = "http://localhost:8000/index.html"
+    fake_page.goto(url)
+    title = fake_page.title()
+    assert title == "MicroPython t-string"
+
+    # Now wait for MicroPython to do its job
+    fake_page.wait_for_selector("span:has-text('Loaded')")
+
+    # See if the first result evaluated to what was expected
+    first_section = fake_page.get_by_title("static_string.string_literal")
+    first_result = first_section.get_by_title("result")
+    first_inner_html = first_result.inner_html()
+    assert first_inner_html == "Hello World"
