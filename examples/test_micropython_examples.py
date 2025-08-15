24f214e94f38d99d56d48408b98ae1ecea719459
pauleveritt <pauleveritt@me.com>
2025-08-15 14:00:15 -0400

    Rebase on the micropython-examples branch, add a fixture that does a pytest fail if the Playwright/Chromium console has errors.

--- examples/test_micropython_examples.py
+++ examples/test_micropython_examples.py
@@ -1,12 +1,16 @@
 """Use Playwright to test examples in MicroPython in a browser."""
-from playwright.sync_api import Page, expect
+
+import pytest
+from playwright.sync_api import Page
 
 
 # Use `PWDEBUG=1` to run "headful" in the Playwright test app
 # @pytest.mark.skip(reason="not implemented")
-def test_static_string(fake_page: Page):
+def test_static_string(fake_page: Page, assert_no_console_errors):
     url = "http://localhost:8000/index.html"
     fake_page.goto(url)
+
+    # First check for errors in the console
     title = fake_page.title()
     assert title == "MicroPython t-string"
 
