902168fac85247ed0dbd9e6ea2f774958cfd2a9b
pauleveritt <pauleveritt@me.com>
2025-08-29 19:27:33 -0400

    Try to address the GHA breakage.

--- examples/components/generators/test_generators.py
+++ examples/components/generators/test_generators.py
@@ -1,12 +1,8 @@
 """Test an example."""
 
-import pytest
-
 from . import main
 
 
-# TODO Andrea this test should pass
-@pytest.mark.skip(reason="Broken in latest generator support")
 def test_main():
     """Ensure the demo matches expected."""
     assert str(main()) == "<ul><li>First</li><li>Second</li></ul>"
