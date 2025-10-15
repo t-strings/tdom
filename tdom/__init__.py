28a04e2d2681949e801badaa4de90db7d71df18c
Stefane Fermigier <sf@fermigier.com>
2025-10-15 21:07:16 +0200

    Add missing sections in README.md. (#65)
    Also, run tests on the README.

--- tdom/__init__.py
+++ tdom/__init__.py
@@ -1,11 +1,13 @@
 from markupsafe import Markup, escape
 
+from .classnames import classnames
 from .nodes import Comment, DocumentType, Element, Fragment, Node, Text
 from .processor import html
 
 # We consider `Markup` and `escape` to be part of this module's public API
 
 __all__ = [
+    "classnames",
     "Comment",
     "DocumentType",
     "Element",
