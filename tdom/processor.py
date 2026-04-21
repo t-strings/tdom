0ef7788c16278eb316073f4fd039b516f9761ae0
Ian Wilson <ian@gardentheory.com>
2026-04-21 12:19:21 -0700

    Flush out simple dependency injection pattern.

--- tdom/processor.py
+++ tdom/processor.py
@@ -519,6 +519,7 @@ class IComponentProcessor(t.Protocol):
         component_callable: ComponentCallable,
         attrs: tuple[TAttribute, ...],
         component_template: Template,
+        provided_attrs: tuple[Attribute, ...] = (),
     ) -> ComponentObject | Template:
         """
         Process available component details into a queryable object or template.
@@ -539,6 +540,7 @@ class ComponentProcessor(IComponentProcessor):
         component_callable: ComponentCallable,
         attrs: tuple[TAttribute, ...],
         component_template: Template,
+        provided_attrs: tuple[Attribute, ...] = (),
     ) -> ComponentObject | Template:
         """
         Process available component details into a queryable object or template.
@@ -554,6 +556,7 @@ class ComponentProcessor(IComponentProcessor):
             get_callable_info(component_callable),
             _resolve_t_attrs(attrs, template.interpolations),
             children=component_template,
+            provided_attrs=provided_attrs,
         )
         return component_callable(**kwargs)
 
