7df7fe8b8c055535a2b93bebf1bad3c6b4983ecd
Dave <davepeck@gmail.com>
2026-05-25 15:14:57 -0700

    Include  and  in

--- tdom/__init__.py
+++ tdom/__init__.py
@@ -1,11 +1,14 @@
 from markupsafe import Markup, escape
 
 from .processor import html, svg
+from .scope import Scope, ScopedTemplate
 
 # We consider `Markup` and `escape` to be part of this module's public API
 
 __all__ = [
     "Markup",
+    "Scope",
+    "ScopedTemplate",
     "escape",
     "html",
     "svg",
