73e24310bc0a99681f0c84fec27666d8d07837f3
Dave <davepeck@gmail.com>
2026-08-19 16:36:25 -0700

    Remove dead code

--- tdom/source.py
+++ tdom/source.py
@@ -9,17 +9,3 @@ class LinePosition:
     " Line of code, starts at 1. "
     offset: int = 0
     " Offset from the start of the line, starts at 0. "
-
-
-@dataclass(slots=True)
-class MutableLinePosition:
-    "A mutable position in a block of source code."
-
-    line: int = 1
-    " Line of code, starts at 1. "
-    offset: int = 0
-    " Offset from the start of the line, starts at 0. "
-
-    def freeze(self) -> LinePosition:
-        "Freeze ourself into an immutable object with the same values."
-        return LinePosition(line=self.line, offset=self.offset)
