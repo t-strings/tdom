7f73d3bcb34290ada19ec4170d40e70c2a2f4467
Dave Peck <davepeck@gmail.com>
2026-08-21 21:01:03 -0700

    Unify docstring syntax; enforce with ruff. (#159)

--- tdom/source.py
+++ tdom/source.py
@@ -3,9 +3,9 @@ from dataclasses import dataclass
 
 @dataclass(slots=True, frozen=True)
 class LinePosition:
-    "A immutable position in a block of source code."
+    """A immutable position in a block of source code."""
 
     line: int = 1
-    " Line of code, starts at 1. "
+    """Line of code, starts at 1."""
     offset: int = 0
-    " Offset from the start of the line, starts at 0. "
+    """Offset from the start of the line, starts at 0."""
